9ea00aeb2a8c46f8af935d0270e58e60f0b148b7
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-08-24 00:22:34 +0700

    Tugas 20 JS

---
+++ Tugas20/tugas20.js
@@ -0,0 +1,4 @@
+let foo = `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`;
+
+console.log(foo.match('are'));
+console.log(foo.match('words'));
