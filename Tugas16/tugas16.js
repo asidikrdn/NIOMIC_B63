e4de3b0dcb74e0868e273e51c0d42edfc8a7523c
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-08-23 23:26:42 +0700

    Tugas 16 JS

---
+++ Tugas16/tugas16.js
@@ -0,0 +1,6 @@
+let deretAngka = [45,100,99,3,2,4,1,2,3,19,22,14,10];
+let nilaiMin = Math.min(...deretAngka);
+let nilaiMax = Math.max(...deretAngka);
+
+console.log('Nilai Minimal : '+nilaiMin);
+console.log('Nilai Maksimal : '+nilaiMax);
