d4438bec7d8098c1f81bdf078c8b1b6c809553d2
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-08-23 22:58:39 +0700

    Tugas 2 JS

---
+++ Tugas2/tugas2.js
@@ -0,0 +1,14 @@
+let variabelSatu = 23;
+let variabelDua = "Ini adalah string";
+let variabelTiga = true;
+let variabelEmpat = null;
+let variabeLima = undefined;
+
+let siswa = ['Ahmad', 'Sidik', 'Rudini', 'Rizki', 'Muhammad'];
+
+console.log(variabelSatu);
+console.log(variabelDua);
+console.log(variabelTiga);
+console.log(variabelEmpat);
+console.log(variabeLima);
+console.log(siswa);
