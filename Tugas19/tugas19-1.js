f416a773f6220cd6486b2556727dcbccb7bd15e7
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-08-24 00:18:58 +0700

    Tugas 19 JS

---
+++ Tugas19/tugas19-1.js
@@ -0,0 +1,6 @@
+let repeatSemangat = (banyakPerunglangan) => {
+  let foo = 'Semangat...';
+  console.log(foo.repeat(banyakPerunglangan));
+}
+
+repeatSemangat(5);
---
+++ Tugas19/tugas19-2.js
@@ -0,0 +1,14 @@
+let repeatCetakBintang = () => {
+  let foo = '*';
+  console.log(foo.repeat(5));
+  console.log(foo.repeat(25));
+  console.log(foo.repeat(45));
+  console.log(foo.repeat(65));
+  console.log(foo.repeat(95));
+  console.log(foo.repeat(65));
+  console.log(foo.repeat(45));
+  console.log(foo.repeat(25));
+  console.log(foo.repeat(5));
+}
+
+repeatCetakBintang();
