62c6dc60b0ec83320739357da21af07fa6a8b87b
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-10-02 11:12:33 +0700

    Tugas 6 ReactJS

---
+++ tugas_6_react_js/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoute from "./AppRoute";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AppRoute />
+  </React.StrictMode>
+);
