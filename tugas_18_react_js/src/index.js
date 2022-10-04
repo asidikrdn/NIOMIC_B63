7715e0f66286fc7ca8edb68c574821124f6e12d2
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2022-10-04 08:33:43 +0700

    Tugas 18 ReactJS

---
+++ tugas_18_react_js/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
